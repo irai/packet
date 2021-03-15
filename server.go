d5bf09a4a45a2ccaaa9680f76c32bdb037accf49
Irai <irai852@gmail.com>
2021-03-15 14:53:31 +1100

    packet: warning if taken more than 30 microseconds

--- server.go
+++ server.go
@@ -458,8 +458,8 @@ func (h *Handler) ListenAndServe(ctxt context.Context) (err error) {
 		}
 
 		elapsed := time.Since(startTime)
-		if elapsed > time.Microsecond*25 {
-			fmt.Printf("packet: warning > 25 microseconds: %v\n", elapsed)
+		if elapsed > time.Microsecond*30 {
+			fmt.Printf("packet: warning > 30 microseconds: %v\n", elapsed)
 		}
 	}
 }
