6c7f6318bdffe747cec7cebfd693c9979d4abf8e
Irai <irai852@gmail.com>
2021-05-06 08:04:51 +1000

    wip

--- test/hosts_test.go
+++ test/hosts_test.go
@@ -1,7 +1,6 @@
 package test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/irai/packet"
@@ -101,52 +100,4 @@ func TestHandler_existingHost(t *testing.T) {
 	}
 
 	checkOnlineCount(t, tc, 3, 2)
-}
-
-func TestHandler_findOrCreateHostDupIP(t *testing.T) {
-	tc := NewTestContext()
-	defer tc.Close()
-
-	packet.Debug = true
-
-	// First create host for IP3 and IP2 and set online
-	host1, _ := tc.engine.Session().FindOrCreateHost(mac1, ip3)
-	engine.lockAndSetOnline(host1, true)
-	host1, _ = engine.findOrCreateHost(mac1, ip2)
-	host1.dhcp4Store.Name = "mac1" // test that name will clear - this was a bug in past
-	engine.lockAndSetOnline(host1, true)
-	engine.lockAndSetOnline(host1, false)
-	if err := engine.Capture(mac1); err != nil {
-		t.Fatal(err)
-	}
-	if !host1.MACEntry.Captured {
-		t.Fatal("host not capture")
-	}
-
-	// new mac, same IP
-	host2, _ := engine.findOrCreateHost(mac2, ip2)
-	if host2.MACEntry.Captured { // mac should not be captured
-		t.Fatal("host not capture")
-	}
-	if host2.dhcp4Store.Name != "" {
-		t.Fatal("invalid host name")
-	}
-
-	// there must be two macs
-	if n := len(engine.MACTable.Table); n != 2 {
-		engine.PrintTable()
-		t.Fatal(fmt.Sprintf("invalid mac table len=%d", n))
-	}
-
-	// The must only be two hosts for IP2
-	if n := len(engine.HostTable.Table); n != 2 {
-		engine.PrintTable()
-		t.Fatal(fmt.Sprintf("invalid host table len=%d ", n))
-	}
-
-	// second IPs
-	host1, _ = engine.findOrCreateHost(mac2, ip2)
-	if host1.MACEntry.Captured { // mac should not be captured
-		t.Fatal("host not capture")
-	}
 }
