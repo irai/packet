ae68159c120af2178b1aa98e51055d818d70d61a
Irai <irai852@gmail.com>
2021-06-16 03:39:57 +1000

    icmp4: remove check for nil mac in sendPacket

--- icmp4/send4.go
+++ icmp4/send4.go
@@ -8,28 +8,21 @@ import (
 )
 
 func (h *Handler) sendPacket(srcAddr packet.Addr, dstAddr packet.Addr, p packet.ICMP4) (err error) {
-	ether := packet.Ether(make([]byte, packet.EthMaxSize)) // Ping is called many times concurrently by client
-
+	ether := packet.Ether(make([]byte, packet.EthMaxSize))
 	ether = packet.EtherMarshalBinary(ether, syscall.ETH_P_IP, srcAddr.MAC, dstAddr.MAC)
 	ip4 := packet.IP4MarshalBinary(ether.Payload(), 50, srcAddr.IP, dstAddr.IP)
 	if ip4, err = ip4.AppendPayload(p, syscall.IPPROTO_ICMP); err != nil {
 		return err
 	}
-
 	if ether, err = ether.SetPayload(ip4); err != nil {
 		return err
 	}
-	if dstAddr.MAC == nil {
-		dstAddr.MAC = packet.Eth4AllNodesMulticast
-	}
-
 	if Debug {
-		fmt.Printf("icmp4: send %s\n", p)
+		fmt.Printf("icmp4 : send %s\n", p)
 	}
 	if _, err := h.session.Conn.WriteTo(ether, &dstAddr); err != nil {
-		fmt.Println("icmp failed to write ", err)
+		fmt.Println("icmp4 : error sending packet ", dstAddr, err)
 		return err
 	}
-
 	return nil
 }
