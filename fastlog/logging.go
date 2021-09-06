a09057b5da4fee7a19f452093666f24fbfbe1d50
Fabio Bastian <irai852@gmail.com>
2021-09-06 11:36:14 +1000

    fastlog: remove reflection in Int funtion

--- fastlog/logging.go
+++ fastlog/logging.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"os"
 	"reflect"
+	"strconv"
 	"sync"
 	"time"
 )
@@ -264,7 +265,7 @@ func (l *Line) Int(name string, value int) *Line {
 	l.index = l.index + copy(l.buffer[l.index:], name)
 	l.buffer[l.index] = '='
 	l.index++
-	l.index = l.index + copy(l.buffer[l.index:], fmt.Sprintf("%d", value))
+	l.index = l.index + copy(l.buffer[l.index:], strconv.Itoa(value))
 	return l
 }
 
@@ -381,33 +382,3 @@ func (l *Line) ByteArray(name string, value []byte) *Line {
 	}
 	return l
 }
-
-/**
-func Strings(data ...string) error {
-	buffer := Std.pool.Get().(*[bufSize]byte)
-	defer Std.pool.Put(buffer)
-	pos := 0
-	for _, v := range data {
-		if pos+len(v) <= cap(buffer) {
-			copy(buffer[pos:], v)
-			pos = pos + len(v)
-			continue
-		}
-		copy(buffer[0:], "MSG TRUNCATED")
-		break
-	}
-	buffer[pos] = '\n'
-	_, err := Std.Out.Write(buffer[:pos+1])
-	return err
-}
-
-func Strings2(str1 string, str2 string) error {
-	buffer := Std.pool.Get().(*[bufSize]byte)
-	defer Std.pool.Put(buffer)
-	pos := copy(buffer[0:], str1)
-	pos = pos + copy(buffer[pos:], str2)
-	buffer[pos] = '\n'
-	_, err := Std.Out.Write(buffer[:pos+1])
-	return err
-}
-*/
