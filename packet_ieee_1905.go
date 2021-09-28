f3b9a6b3e952c9500247b72ded5d50532f58b012
Fabio Bastian <irai852@gmail.com>
2021-09-28 15:26:07 +1000

    engine: parse ethernet pause flow control

--- packet_ieee_1905.go
+++ packet_ieee_1905.go
@@ -37,3 +37,30 @@ func (p IEEE1905) FastLog(line *fastlog.Line) *fastlog.Line {
 	line.ByteArray("tlv", p.TLV())
 	return line
 }
+
+type EthernetPause []byte
+
+func (p EthernetPause) IsValid() error {
+	if len(p) < 46 {
+		return ErrFrameLen
+	}
+	if p.Opcode() != 0x0001 {
+		return ErrParseFrame
+	}
+	return nil
+}
+
+func (p EthernetPause) Opcode() uint16   { return binary.BigEndian.Uint16(p[0:2]) } // must be 0x0001
+func (p EthernetPause) Duration() uint16 { return binary.BigEndian.Uint16(p[2:4]) } // typically 0x0000 or 0xffff
+func (p EthernetPause) Reserved() []byte { return p[4:] }
+
+func (p EthernetPause) String() string {
+	line := fastlog.NewLine("", "")
+	return p.FastLog(line).ToString()
+}
+
+func (p EthernetPause) FastLog(line *fastlog.Line) *fastlog.Line {
+	line.Uint16Hex("opcode", p.Opcode())
+	line.Uint16Hex("duration", p.Duration())
+	return line
+}
