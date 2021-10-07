c854b6f62fd915089eb060966bc6b0928176e246
Fabio Bastian <irai852@gmail.com>
2021-10-07 23:13:15 +1100

    icmp4: test packets

---
+++ icmp4/icmp4_test.go
@@ -0,0 +1,16 @@
+package icmp4
+
+/**
+
+TODO: parse ICMP4 router advertisement packet = type 9
+http://networksorcery.com/enp/protocol/icmp/msg9.htm
+
+icmp4 not implemented type=9: frame:0x[74 79 70 65 3d 39 20 63 6f 64 65 3d 30 20 70 61 79 6c 6f 61 64 4c 65 6e 3d 31 38 2c 20 64 61 74 61 3d 30 78 63 30 20 61 38 20 30 30 20 30 31 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30]
+Oct 07 03:52:24 netfilter netfilter[13069]: icmp4 not implemented type=9: frame:0x[74 79 70 65 3d 39 20 63 6f 64 65 3d 30 20 70 61 79 6c 6f 61 64 4c 65 6e 3d 31 38 2c 20 64 61 74 61 3d 30 78 63 30 20 61 38 20 30 30 20 30 31 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30]
+Oct 07 03:52:24 netfilter netfilter[13069]: icmp4 not implemented type=9: frame:0x[74 79 70 65 3d 39 20 63 6f 64 65 3d 30 20 70 61 79 6c 6f 61 64 4c 65 6e 3d 31 38 2c 20 64 61 74 61 3d 30 78 32 38 20 30 34 20 30 31 20 34 64 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30]
+Oct 07 03:52:24 netfilter netfilter[13069]: icmp4 not implemented type=9: frame:0x[74 79 70 65 3d 39 20 63 6f 64 65 3d 30 20 70 61 79 6c 6f 61 64 4c 65 6e 3d 31 38 2c 20 64 61 74 61 3d 30 78 32 38 20 30 34 20 30 31 20 34 64 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30 20 30 30]
+Oct 07 03:52:24 netfilter netfilter[13069]: packet: "ipv4 igmp packet" type=0x0800 src=6a:02:b8:ac:79:fd dst=01:00:5e:00:00:16 len=60
+Oct 07 03:52:24 netfilter netfilter[13069]: packet: "ipv4 igmp packet" type=0x0800 src=6a:02:b8:ac:79:fd dst=01:00:5e:00:00:16 len=60
+Oct 07 03:52:25 netfilter netfilter[13069]: packet: "ipv4 igmp packet" type=0x0800 src=6a:02:b8:ac:79:fd dst=01:00:5e:00:00:16 len=60
+Oct 07 03:52:25 netfilter netfilter[13069]: packet: "ipv4 igmp packet" type=0x0800 src=6a:02:b8:ac:79:fd dst=01:00:5e:00:00:16 len=60
+*/
